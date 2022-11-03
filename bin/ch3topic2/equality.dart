509eafe0dc81727965889bf8bb4801ac5d7bd801
bahri <saiful.bahri.tl@gmail.com>
2022-11-03 23:22:19 +0700

    equality penjelasan

--- bin/ch3topic2/equality.dart
+++ bin/ch3topic2/equality.dart
@@ -19,3 +19,39 @@ class Youtube {
   @override
   int get hashCode => email.hashCode ^ youtubeChannel.hashCode;
 }
+
+class YoutubeWithoutEquality {
+  final String email;
+  final String youtubeChannel;
+
+  const YoutubeWithoutEquality({
+    required this.email,
+    required this.youtubeChannel,
+  });
+}
+
+void main(List<String> args) {
+  Youtube bahri = Youtube(
+    email: 'bahri@gmail.com',
+    youtubeChannel: 'code with bahri',
+  );
+
+  Youtube bahriDuplicate = Youtube(
+    email: 'bahri@gmail.com',
+    youtubeChannel: 'code with bahri',
+  );
+
+  print(bahri == bahriDuplicate);
+
+  YoutubeWithoutEquality bahriWithoutEquality = YoutubeWithoutEquality(
+    email: 'bahri@gmail.com',
+    youtubeChannel: 'code with bahri',
+  );
+
+  YoutubeWithoutEquality bahriDuplicateWithoutEquality = YoutubeWithoutEquality(
+    email: 'bahri@gmail.com',
+    youtubeChannel: 'code with bahri',
+  );
+
+  print(bahriWithoutEquality == bahriDuplicateWithoutEquality);
+}

87c113af76bc0631e2ca66f63d55b884eb13bcf8
bahri <saiful.bahri.tl@gmail.com>
2022-11-14 09:27:54 +0700

    update code

--- bin/bank_soal/ganjil.dart
+++
@@ -1,9 +0,0 @@
-//topic 1, code gym 2
-void main(List<String> args) {
-  int bilangan = 4;
-  if (bilangan % 2 == 1) {
-    print('$bilangan adalah ganjil');
-  } else {
-    print('$bilangan tidak ganjil');
-  }
-}
--- bin/bank_soal/perulangan.dart
+++
@@ -1,7 +0,0 @@
-//topic 1, code gym 1
-
-void main() {
-  for (int x = 10; x >= 0; x--) {
-    print('bilangan $x');
-  }
-}
